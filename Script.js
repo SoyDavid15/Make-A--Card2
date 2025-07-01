5d2ebe0c3c6695dd2a554e083d0ab71809ff3a24
Samuel Ordoñez <kaynower@gmail.com>
2025-07-01 18:48:54 -0500

    Se agrego la interaccion de ir de una pagina a otra

--- Script.js
+++ Script.js
@@ -1,22 +1,6 @@
-var BotonCrearCarta = document.querySelector(".btn-personalizado");
-var Titulo = document.querySelector("h1 span");
+var BotonCrear = document.querySelector(".buttonCrear")
 
-// Obtener la fuente original correctamente desde el CSS
-var fuenteOriginal = window.getComputedStyle(Titulo).fontFamily;
-
-BotonCrearCarta.addEventListener("click", function() {
+BotonCrear.addEventListener("click",function()
+{
     window.location.href = "CardSite.html";
 });
-
-function alternarFuente() {
-    // Si la fuente actual es la original, cambia a "Pixelify Sans"
-    if (Titulo.style.fontFamily === fuenteOriginal || Titulo.style.fontFamily === '') {
-        Titulo.style.fontFamily = "Pixelify Sans";
-    } else {
-        // Si la fuente actual es "Pixelify Sans", cambia a la fuente original
-        Titulo.style.fontFamily = fuenteOriginal;
-    }
-}
-
-// Alternar la fuente cada 1 segundo
-setInterval(alternarFuente, 1000);
