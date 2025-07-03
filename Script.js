bff85250460902b7f894d4c3f64405cb8e8e4943
SoyDavid15 <77743709+SoyDavid15@users.noreply.github.com>
2025-07-02 19:20:17 -0500

    V2.0
    Se hizo una pantalla de presentacion

--- Script.js
+++ Script.js
@@ -1,8 +1,12 @@
-var Sobre = document.querySelector(".Sobre");
-var TextArea = Sobre.querySelector("#description");
+let BotonCrear = document.querySelector(".buttonCrear")
+let BotonGuia = document.querySelector(".buttonGuia")
 
-var Boton = document.querySelector(".Enviar");
-Boton.addEventListener("click", function() {
-    TextArea.value = "Te amo <3\nGracias por todo lo que haces por mí.\nEres la mejor persona del mundo.\nTe amo mucho, mucho, mucho.\nGracias por aguantarme tanto\nEres la persona más increíble que he conocido.\nTe amo con todo mi corazón.\nSiempre estaré aquí para ti.\nTe amo, te amo, te amo.\nEres mi todo.";
-    alert("Vuelve a ver la carta")
+BotonCrear.addEventListener("click",function()
+{
+    window.location.href = "CardSite.html";
+});
+
+BotonGuia.addEventListener("click",function()
+{
+    window.location.href = "#PaginaGuia";
 });
