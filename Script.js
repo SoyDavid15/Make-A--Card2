6b0cf500cc4e68747fded4c2f1637cffb8758184
Samuel Ordoñez <kaynower@gmail.com>
2025-07-02 19:15:05 -0500

    Primera version de la página interactiva

--- Script.js
+++ Script.js
@@ -1,6 +1,12 @@
-var BotonCrear = document.querySelector(".buttonCrear")
+let BotonCrear = document.querySelector(".buttonCrear")
+let BotonGuia = document.querySelector(".buttonGuia")
 
 BotonCrear.addEventListener("click",function()
 {
     window.location.href = "CardSite.html";
+});
+
+BotonGuia.addEventListener("click",function()
+{
+    window.location.href = "#PaginaGuia";
 });
